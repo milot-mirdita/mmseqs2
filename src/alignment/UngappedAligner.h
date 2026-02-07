5838c7c1be9d7bbe1d09a4efac80de980d53b421
Milot Mirdita <milot@mirdita.de>
2026-02-07 17:16:54 +0900

    Fix AVX512 checks on old clang

--- src/alignment/UngappedAligner.h
+++ src/alignment/UngappedAligner.h
@@ -52,15 +52,23 @@ UngappedAligner::Impl *createImpl(size_t maxSequenceLength,
 inline UngappedAligner::UngappedAligner(size_t maxSequenceLength, int aaSize, bool aaBiasCorrection, float aaBiasCorrectionScale)
     : impl(nullptr) {
 #if (defined(__x86_64__) || defined(__i386__) || defined(_M_X64) || defined(_M_IX86)) && (defined(__GNUC__) || defined(__clang__))
+    bool avx512_supported = false;
+#if defined(__has_builtin)
+#if __has_builtin(__builtin_cpu_supports)
+#if __has_builtin(__builtin_cpu_init)
     __builtin_cpu_init();
-    if (__builtin_cpu_supports("avx512f") && __builtin_cpu_supports("avx512bw") && __builtin_cpu_supports("avx512dq")) {
-        impl = UngappedAVX512::createImpl(maxSequenceLength, aaSize, aaBiasCorrection, aaBiasCorrectionScale);
-    } else {
-        impl = UngappedDefault::createImpl(maxSequenceLength, aaSize, aaBiasCorrection, aaBiasCorrectionScale);
-    }
-#else
-    impl = UngappedDefault::createImpl(maxSequenceLength, aaSize, aaBiasCorrection, aaBiasCorrectionScale);
 #endif
+    avx512_supported = __builtin_cpu_supports("avx512f") &&
+                       __builtin_cpu_supports("avx512bw") &&
+                       __builtin_cpu_supports("avx512dq");
+#endif
+#endif
+    if (avx512_supported) {
+        impl = UngappedAVX512::createImpl(maxSequenceLength, aaSize, aaBiasCorrection, aaBiasCorrectionScale);
+        return;
+    }
+#endif
+    impl = UngappedDefault::createImpl(maxSequenceLength, aaSize, aaBiasCorrection, aaBiasCorrectionScale);
 }
 
 #endif
